903c9af196fedb29ee7ea0d83832441327055643
Hamilton Chapman <hamchapman@gmail.com>
2016-11-16 15:22:45 +0000

    Rename LiveListsHelper FeedsHelper

--- Source/FeedsHelper.swift
+++ Source/FeedsHelper.swift
@@ -1,12 +1,12 @@
 //
-//  ElementsLiveListsHelper.swift
+//  FeedsHelper.swift
 //  ElementsSwift
 //
 //  Created by Hamilton Chapman on 05/10/2016.
 //
 //
 
-@objc public class LiveListsHelper: NSObject, ServiceHelper {
+@objc public class FeedsHelper: NSObject, ServiceHelper {
     public var app: ElementsApp
 
     public init(app: ElementsApp) {
