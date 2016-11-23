264212f75347a59e0e47408ec756061b2a5e03b9
Hamilton Chapman <hamchapman@gmail.com>
2016-11-23 11:13:45 +0000

    EndpointAuthorizer caches received jwt

--- Source/EndpointAuthorizer.swift
+++ Source/EndpointAuthorizer.swift
@@ -11,6 +11,7 @@ import PromiseKit
 public class EndpointAuthorizer: Authorizer {
     public var url: String
     public var requestMutator: ((URLRequest) -> (URLRequest))?
+    public var jwt: String? = nil
 
     public init(url: String, requestMutator: ((URLRequest) -> (URLRequest))? = nil) {
         self.url = url
@@ -18,57 +19,64 @@ public class EndpointAuthorizer: Authorizer {
     }
 
     public func authorize() -> Promise<String> {
-        return Promise { fulfill, reject in
-            var request = URLRequest(url: URL(string: url)!)
-            request.httpMethod = "POST"
-
-            // TODO: Why is this an empty string?
-            request.httpBody = "".data(using: String.Encoding.utf8)
-
-            if requestMutator != nil {
-                request = requestMutator!(request)
+        if jwt != nil {
+            return Promise { resolve, reject in
+                resolve(self.jwt!)
             }
+        } else {
+            return Promise { fulfill, reject in
+                var request = URLRequest(url: URL(string: url)!)
+                request.httpMethod = "POST"
 
-            URLSession.shared.dataTask(with: request, completionHandler: { data, response, sessionError in
-                if let error = sessionError {
-                    reject(error)
-                    return
+                // TODO: Why is this an empty string?
+                request.httpBody = "".data(using: String.Encoding.utf8)
+
+                if requestMutator != nil {
+                    request = requestMutator!(request)
                 }
 
-                guard let data = data else {
-                    reject(EndpointAuthorizerError.noDataPresent)
-                    return
-                }
+                URLSession.shared.dataTask(with: request, completionHandler: { data, response, sessionError in
+                    if let error = sessionError {
+                        reject(error)
+                        return
+                    }
 
-                let dataString = String(data: data, encoding: String.Encoding.utf8)
+                    guard let data = data else {
+                        reject(EndpointAuthorizerError.noDataPresent)
+                        return
+                    }
 
-                guard let httpResponse = response as? HTTPURLResponse else {
-                    // TODO: Print dataString somewhere sensible
-                    print("Invalid response object, data: \(dataString)")
-                    reject(EndpointAuthorizerError.invalidHttpResponse(data: data))
-                    return
-                }
+                    let dataString = String(data: data, encoding: String.Encoding.utf8)
 
-                guard 200..<300 ~= httpResponse.statusCode else {
-                    // TODO: Print dataString somewhere sensible
-                    print("Bad status code, data: \(dataString)")
-                    reject(EndpointAuthorizerError.badResponseStatusCode(response: httpResponse, data: data))
-                    return
-                }
+                    guard let httpResponse = response as? HTTPURLResponse else {
+                        // TODO: Print dataString somewhere sensible
+                        print("Invalid response object, data: \(dataString)")
+                        reject(EndpointAuthorizerError.invalidHttpResponse(data: data))
+                        return
+                    }
 
-                guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []), let json = jsonObject as? [String: String] else {
-                    // TODO: Log what was trying to be deserialzied here or in catch block where this is called
-                    reject(EndpointAuthorizerError.unableToDeserializeJsonResponse)
-                    return
-                }
+                    guard 200..<300 ~= httpResponse.statusCode else {
+                        // TODO: Print dataString somewhere sensible
+                        print("Bad status code, data: \(dataString)")
+                        reject(EndpointAuthorizerError.badResponseStatusCode(response: httpResponse, data: data))
+                        return
+                    }
 
-                guard json["jwt"] != nil else {
-                    reject(EndpointAuthorizerError.jwtKeyNotPresentInResponse)
-                    return
-                }
+                    guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []), let json = jsonObject as? [String: String] else {
+                        // TODO: Log what was trying to be deserialzied here or in catch block where this is called
+                        reject(EndpointAuthorizerError.unableToDeserializeJsonResponse)
+                        return
+                    }
 
-                fulfill(json["jwt"]!)
-            })
+                    guard json["jwt"] != nil else {
+                        reject(EndpointAuthorizerError.jwtKeyNotPresentInResponse)
+                        return
+                    }
+
+                    self.jwt = json["jwt"]!
+                    fulfill(json["jwt"]!)
+                })
+            }
         }
     }
 }
