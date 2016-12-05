f9a79d99d74d43f258816b78067f9e15b2b37aec
Hamilton Chapman <hamchapman@gmail.com>
2016-12-05 11:36:53 +0000

    Remove comments at top of FeedsHelper

--- Source/FeedsHelper.swift
+++ Source/FeedsHelper.swift
@@ -1,11 +1,3 @@
-//
-//  FeedsHelper.swift
-//  ElementsSwift
-//
-//  Created by Hamilton Chapman on 05/10/2016.
-//
-//
-
 import PromiseKit
 
 @objc public class FeedsHelper: NSObject, ServiceHelper {
