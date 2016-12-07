42d2bc4ea9d37fffb9c874ab089a16d17b1bdd3d
Zan Markan <zan@pusher.com>
2016-12-07 09:58:54 +0000

    Subscribe to a feed in sample iOS viewController

--- Elements macOS Example/Elements iOS Example/Elements iOS Example/FeedsViewController.swift
+++ Elements macOS Example/Elements iOS Example/Elements iOS Example/FeedsViewController.swift
@@ -11,14 +11,27 @@ import UIKit
 
 class FeedsViewController: UIViewController {
     
+    @IBOutlet var feedLabel: UILabel!
+    
+    var delegate: AppDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Feeds ViewController")
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        delegate = UIApplication.shared.delegate as! AppDelegate
+
+        let feed = delegate.elements.feeds("resumable-newer")
+        
+        try! feed.subscribeWithResume(
+            onOpen: { Void in print("We're open") },
+            onAppend: { itemId, headers, item in print("RECEIVED", itemId, headers, item) } ,
+            onEnd: { statusCode, headers, info in print("END", statusCode, headers, info) },
+            onStateChange: { oldState, newState in print("was \(oldState) now \(newState)") }
+            ).then { resSub -> Void in
+                print("Subscribed!")
+            }.catch { error in
+                print(error)
+        }
     }
 }
