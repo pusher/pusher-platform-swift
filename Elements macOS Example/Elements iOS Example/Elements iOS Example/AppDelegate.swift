3a005aceadf4c1d32faad360cfcccddd897a7122
Hamilton Chapman <hamchapman@gmail.com>
2016-12-21 11:19:26 +0000

    Remove all UserNotfications related code

--- Elements macOS Example/Elements iOS Example/Elements iOS Example/AppDelegate.swift
+++ Elements macOS Example/Elements iOS Example/Elements iOS Example/AppDelegate.swift
@@ -8,35 +8,18 @@
 
 import UIKit
 import ElementsSwift
-import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     public var elements: App!
-    public var notificationsHelper: UserNotificationsHelper?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        let authorizer = SimpleTokenAuthorizer(jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJncmFudHMiOnsiL2FwcHMvKi91c2VyLW5vdGlmaWNhdGlvbnMvKioiOlsiKiJdfSwiaXNzIjoiNjRlYzNkOGMtYTc4NS00M2Q1LTk4YTUtZTYxNDViYTEzNDgwIn0.QkPB2acA9HrkFytmcXxeSbyPXyoAz2-OmSTQ7lBQCn4")
-        elements = try! App(id: "3", authorizer: authorizer)
-        notificationsHelper = elements.userNotifications(userId: "zan")
-        
-        let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options:[.badge, .alert, .sound]) { (granted, error) in
-            guard granted else {
-                return
-            }
-            application.registerForRemoteNotifications()
-        }
+        let authorizer = SimpleTokenAuthorizer(jwt: "some.relevant.jwt")
+        elements = try! App(id: "yourAppId", authorizer: authorizer)
         return true
-    }
-    
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        
-        try! notificationsHelper?.register(deviceToken: deviceToken)
     }
 }
 
--- Elements macOS Example/Elements macOS Example/AppDelegate.swift
+++ Elements macOS Example/Elements macOS Example/AppDelegate.swift
@@ -7,32 +7,12 @@
 //
 
 import Cocoa
-import ElementsSwift
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    public var elements: App!
-//    public var notificationsHelper: UserNotificationsHelper?
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        
-//        let authorizer = SimpleTokenAuthorizer(jwt: "YOUR.CLIENT.JWT")
-//        elements = try! App(id: "3", authorizer: authorizer)
-//
-//        notificationsHelper = elements.userNotifications(userId: "zan")
-//        
-//        NSApplication.shared().registerForRemoteNotifications(matching: [.alert, .sound])
-    }
+    func applicationDidFinishLaunching(_ aNotification: Notification) {}
 
     func applicationWillTerminate(_ aNotification: Notification) {}
-    
-    func application(_ application: NSApplication, didReceiveRemoteNotification userInfo: [String : Any]) {
-        print("Received remote notification: \(userInfo)")
-    }
-    
-    func application(_ application: NSApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-//        try! notificationsHelper?.register(deviceToken: deviceToken)
-    }
 
 }
