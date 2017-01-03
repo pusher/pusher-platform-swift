dd0b410a256261cef0ede78b449d17748019cc25
Hamilton Chapman <hamchapman@gmail.com>
2017-01-03 11:38:58 +0000

    Add Logger

---
+++ Source/Logger.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+@objc public protocol Logger: class {
+    func log(message: String)
+}
+
+@objc class DefaultLogger: NSObject, Logger {
+    static public var Logger: Logger = DefaultLogger()
+    internal let logQueue = DispatchQueue(label: "com.pusher.elements-client-swift.logger")
+
+    public func log(message: String) {
+        logQueue.async {
+            print(message)
+        }
+    }
+}
