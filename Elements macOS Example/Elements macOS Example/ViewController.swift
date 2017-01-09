9251116cf194c7b8692b162d34a4c444fd18e2ca
Hamilton Chapman <hamchapman@gmail.com>
2017-01-09 14:24:10 +0000

    Update macOS example to do more

--- Elements macOS Example/Elements macOS Example/ViewController.swift
+++ Elements macOS Example/Elements macOS Example/ViewController.swift
@@ -10,39 +10,105 @@ import Cocoa
 import ElementsSwift
 
 class ViewController: NSViewController {
-    var elements: App!
+    var app: App? = nil
+    var feed: FeedsHelper? = nil
+    var feedItems: [[String: String]] = []
 
-    override func viewDidLoad() {
-        let authorizer = SimpleTokenAuthorizer(jwt: "some.relevant.jwt")
-        
-        elements = try! App(id: "yourAppId", authorizer: authorizer)
+    @IBOutlet weak var feedTableView: NSTableView!
 
-        let resumable = elements.feeds("resumable-ham")
-
-        resumable.subscribeWithResume(
+    @IBAction func subscribeButton(_ sender: Any) {
+        feed?.subscribeWithResume(
             onOpen: { Void in print("OPEN") },
-            onAppend: { itemId, headers, item in print("RECEIVED: ", itemId, headers, item) } ,
+            onAppend: { itemId, headers, item in
+                print("RECEIVED: ", itemId, headers, item)
+                if let item = item as? [String: String] {
+                    self.feedItems.append(item)
+                    self.reloadFeedTableView()
+                } else {
+                    print("Item not formed correctly: \(item)")
+                }
+            },
             onEnd: { statusCode, headers, info in print("END: ", statusCode, headers, info) },
             onError: { error in print("ERROR: ", error) },
             onStateChange: { oldState, newState in print("was \(oldState) now \(newState)") }
-        ) { result in
-                print("RESULT: ", result)
+        )
+    }
+
+    @IBAction func unsubscribeButton(_ sender: Any) {
+        feed?.unsubscribe()
+    }
+
+    @IBAction func fetchOlderItemsButton(_ sender: Any) {
+        feed?.fetchOlderItems() { result in
+            switch result {
+            case .failure(let error):
+                print("Error when fetching older items: \(error)")
+            case .success(let items):
+                print("***********************")
+                print(items)
+                let castItems = items.reversed().flatMap { $0["data"] as? [String: String] }
+                self.feedItems = castItems + self.feedItems
+                self.reloadFeedTableView()
+            }
         }
+    }
 
-//        resumable.get(limit: 10) { result in
-//            print("Got a result, and here it is: \(result)")
-//            print(result.value?.items)
-//            print(result.value?.nextId)
-//        }
-//
-//        resumable.append(item: "testing") { result in
-//            print("Got a result, and here it is: \(result)")
-//            print(result.value)
-//        }
+    @IBAction func appendRandomDataButton(_ sender: Any) {
+        feed?.append(item: ["timestamp": String(Int(Date().timeIntervalSince1970)), "uuid": NSUUID().uuidString])
+    }
 
+    func reloadFeedTableView() {
+        DispatchQueue.main.async {
+            print("About to reload table view")
+            self.feedTableView.reloadData()
+        }
+    }
+
+    override func viewDidLoad() {
+        self.feedTableView.dataSource = self
+        self.feedTableView.delegate = self
+
+        let authorizer = SimpleTokenAuthorizer(jwt: "some.relevant.jwt")
+        app = try! App(id: "yourAppId", authorizer: authorizer)
+        feed = app?.feeds("resumable-ham")
     }
 
     override var representedObject: Any? {
         didSet {}
+    }
+}
+
+extension ViewController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return self.feedItems.count
+    }
+}
+
+extension ViewController: NSTableViewDelegate {
+    fileprivate enum CellIdentifiers {
+        static let TimestampCell = "TimestampCellID"
+        static let UUIDCell = "UUIDCellID"
+    }
+
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        var text: String = ""
+        var cellIdentifier: String = ""
+
+        let item = feedItems[row]
+
+        if tableColumn == tableView.tableColumns[0] {
+            text = item["timestamp"]!
+            cellIdentifier = CellIdentifiers.TimestampCell
+        } else if tableColumn == tableView.tableColumns[1] {
+            text = item["uuid"]!
+            cellIdentifier = CellIdentifiers.UUIDCell
+        }
+
+        if let cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+            return cell
+        }
+
+        return nil
     }
 }
