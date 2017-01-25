2a0021da49755dd6dcdfd452af4f16c31ae27537
Hamilton Chapman <hamchapman@gmail.com>
2017-01-25 16:19:53 +0000

    Make DummyTests work

---
+++ Tests/DummyTests.swift
@@ -0,0 +1,12 @@
+import PusherPlatform
+import XCTest
+
+class DummyTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    func testSubscribingToAPrivateChannelShouldMakeARequestToTheAuthEndpoint() {
+        XCTAssertTrue(2 == 1 + 1, "maths")
+    }
+}
