1af4955f7ae8a999c20930798077cb4c5d48f991
Luka Bratos <luka@pusher.com>
2017-07-20 12:17:21 +0100

    Set client version in init

---
+++ Source/ClientVersion.swift
@@ -0,0 +1,9 @@
+
+class ClientVersion {
+    class func shortVersion() -> String {
+        let bundle = Bundle(identifier: "com.pusher.PusherPlatform")!
+        let shortVersion = bundle.infoDictionary?["CFBundleShortVersionString"] as? String
+
+        return shortVersion!
+    }
+}
