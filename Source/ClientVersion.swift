07c564b9e714fc1ec1b2ffda438871bb0376aab5
Luka Bratos <luka@pusher.com>
2017-07-20 14:21:19 +0100

    Refactor

--- Source/ClientVersion.swift
+++ Source/ClientVersion.swift
@@ -1,8 +1,6 @@
 final class ClientVersion {
     class func shortVersion() -> String {
         let bundle = Bundle(identifier: "com.pusher.PusherPlatform")!
-        let shortVersion = bundle.infoDictionary?["CFBundleShortVersionString"] as? String
-
-        return shortVersion!
+        return bundle.infoDictionary!["CFBundleShortVersionString"] as! String
     }
 }
