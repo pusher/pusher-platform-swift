85f22a3f9bb43ffad3e3160925a7e335b003fb94
Luka Bratos <luka@pusher.com>
2017-09-04 11:33:43 +0100

    Remove ClientVersion class

--- Source/ClientVersion.swift
+++
@@ -1,6 +0,0 @@
-final class ClientVersion {
-    class func shortVersion() -> String {
-        let bundle = Bundle(identifier: "com.pusher.PusherPlatform")!
-        return bundle.infoDictionary!["CFBundleShortVersionString"] as! String
-    }
-}
