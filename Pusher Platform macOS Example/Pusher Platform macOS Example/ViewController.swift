eb644e902c9718385d56bffeacd381242d49cade
Hamilton Chapman <hamchapman@gmail.com>
2018-01-09 15:28:06 +0000

    Update examples

--- Pusher Platform macOS Example/Pusher Platform macOS Example/ViewController.swift
+++ Pusher Platform macOS Example/Pusher Platform macOS Example/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Pusher Platform macOS Example
-//
-//  Created by Hamilton Chapman on 27/09/2016.
-//  Copyright © 2016 Pusher. All rights reserved.
-//
-
 import Cocoa
 import PusherPlatform
 
